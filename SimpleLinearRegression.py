49ec05143ed7d75779d811f1912673c8b2bc4493
AhmetMert1907 <36266367+AhmetMert1907@users.noreply.github.com>
2018-02-08 15:07:17 +0200

    Update SimpleLinearRegression.py

--- SimpleLinearRegression.py
+++ SimpleLinearRegression.py
@@ -1,8 +1,8 @@
-#Importing Library(Kütüphanelerin eklenmesi)
+#Importing Library(Kutuphanelerin eklenmesi)
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Creation of data lists(veri listelerinin oluşturulması)
+#Creation of data lists(veri listelerinin olusturulmasi)
 x = np.array([1,2,3,4,5])
 y = np.array([1.5,0.8,1.7,3.8,7.6])
 
@@ -12,7 +12,7 @@ B = np.vstack([x,np.ones(len(x))]).T
 #finds m and c in function(mx+c=y fonksiyonundaki m ve c yi bulur) 
 m , c = np.linalg.lstsq(B,y)[0]
 
-#finally , to see what I'm doing in the chart(son olarak , grafikte ne yaptığımı görebilmek için)
+#finally , to see what I'm doing in the chart(son olarak , grafikte ne yaptigimi görebilmek icin)
 plt.plot(x,y,'o', label='Orijinal veri', markersize=8)
 plt.plot(x,m*x+c,'r',label='Uydurulmuş Veri')
 plt.legend()
